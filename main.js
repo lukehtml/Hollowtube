a0b3cb238d2c9e49723cd2ac5b730e47690b0abe
luke the meow man <kedoooooooooooo@gmail.com>
2024-12-11 12:56:14 +0100

    Undo update main.js

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-let img = [
+let pics = [
 "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/256742997/movie.293x165.jpg?t=1550167436"
 ];
 
-function updateAllThumbnails() {
-    const thumbnails = document.querySelectorAll("img");
+function updateAllImages() {
+    const imgs = document.querySelectorAll("img");
 
     imgs.forEach(image => {
-        image.src = img[1];
+        image.src = pics[Math.floor(Math.random() * pics.length)];
     });
 }
 
 setInterval(updateAllImages, 500);
-
 
